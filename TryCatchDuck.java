1c0babbaf8ace95f28cfd22ca2c9d45034da78c9
ShadeEmsigima <thisisforlis@duck.com>
2023-02-21 11:00:31 +0100

    duck mechanics completed

--- TryCatchDuck.java
+++ TryCatchDuck.java
@@ -1,26 +1,27 @@
 class TryCatchDuck {
     public static void main(String[] args) {
-        System.out.println("You are a proud owner of a snow-white pet duck.");
+        System.out.println("\nYou are a proud owner of a snow-white pet duck");
 
         try{
-            System.out.println("You approach your duck.");
-            ApproachDuck("back");
-            System.out.println("You try to pick up your duck.");
-            //PickUpDuck(900);
+            System.out.println("You approach your duck");
+            ApproachDuck("front");
+
+            System.out.println("You place your palm firmly under its soft belly");
+            PickUpDuck(1);
         }
         catch (DuckIsScaredException a){
             System.out.println("The duck is too scared, and waddles away from you!");
         }
         catch (DuckIsTooHeavyException b){
-            System.out.println("The duck is too heavy to lift! Leave it on the ground.");
+            System.out.println("The duck is too heavy to lift! You leave it on the ground");
         } finally{
-            System.out.println("You will always love your duckie.");
+            System.out.println("You'll always love your duckie");
         }
 
         System.out.println();
     }
 
-    static void ApproachDuck(String direction) throws Exception{
+    static void ApproachDuck(String direction) throws DuckIsScaredException{
         if (direction.equalsIgnoreCase("front"))
             System.out.println("Duck stands still for you obediently");
         else{
@@ -28,7 +29,11 @@ class TryCatchDuck {
         }
     }
 
-    static void PickUpDuck(int duckWeight) throws Exception{
-
+    static void PickUpDuck(int duckWeight) throws DuckIsTooHeavyException{
+        if (duckWeight > 20)
+        throw new DuckIsTooHeavyException();
+        else {
+            System.out.println("You are now successfully holding the duck in your hand!");
+        }
     }
 }
