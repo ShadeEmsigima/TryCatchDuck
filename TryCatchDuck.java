e60a28a1aa178a2210bfbd6fc3bbe3c5a6c42ee6
ShadeEmsigima <thisisforlis@duck.com>
2023-02-22 09:16:24 +0100

    updating exceptions

--- TryCatchDuck.java
+++ TryCatchDuck.java
@@ -1,23 +1,23 @@
 class TryCatchDuck {
     public static void main(String[] args) {
         System.out.println("\nYou are a proud owner of a snow-white pet duck");
-
+        
         try{
             System.out.println("You approach your duck");
             ApproachDuck("front");
-
+            
             System.out.println("You place your palm firmly under its soft belly");
             PickUpDuck(1);
         }
-        catch (DuckIsScaredException a){
+        catch (DuckIsScaredException dise){
             System.out.println("The duck is too scared, and waddles away from you!");
         }
-        catch (DuckIsTooHeavyException b){
+        catch (DuckIsTooHeavyException dithe){
             System.out.println("The duck is too heavy to lift! You leave it on the ground");
         } finally{
             System.out.println("You'll always love your duckie");
         }
-
+        
         System.out.println();
     }
 
